3291384a4e19954461f67e3db312e9d11a10dc67
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-05-16 18:04:55 +0200

    almost finished

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+gulp.task('styles', () => {
+	return gulp.src('scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css/'));
+});
+
+gulp.task('clean', () => {
+	return del([
+		'css/main.css',
+	]);
+});
+
+gulp.task('watch', () => {
+	gulp.watch('scss/**/*.scss', (done) => {
+		gulp.series(['clean', 'styles'])(done);
+	});
+});
+
+//gulp.task('default', gulp.series(['clean', 'styles']));
+gulp.task('default', gulp.series(['watch']));
